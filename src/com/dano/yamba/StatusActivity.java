e6d2740613206202e3813639976f7df55c6bd5ed
Daniela Zapata <zapidan@gmail.com>
2014-01-18 17:50:15 -0600

    Add basic UI

--- src/com/dano/yamba/StatusActivity.java
+++ src/com/dano/yamba/StatusActivity.java
@@ -1,15 +1,37 @@
 package com.dano.yamba;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class StatusActivity extends Activity {
+public class StatusActivity extends Activity implements OnClickListener {
+	
+	private static final String TAG = "StatusActivity";
+	private EditText editStatus;
+	private Button buttonTweet;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_status);
+		
+		editStatus = (EditText)findViewById(R.id.editStatus);
+		buttonTweet = (Button)findViewById(R.id.buttonTweet);
+		
+		buttonTweet.setOnClickListener(this);
+		
+	}
+	
+	@Override
+	public void onClick(View view) {
+		String status = editStatus.getText().toString();
+		
+		Log.d(TAG, "onClicked with status: " + status);
 	}
 
 	@Override
@@ -18,5 +40,6 @@ public class StatusActivity extends Activity {
 		getMenuInflater().inflate(R.menu.status, menu);
 		return true;
 	}
+	
 
 }
