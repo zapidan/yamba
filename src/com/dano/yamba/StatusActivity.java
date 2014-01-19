93e9fb28b7185c8c65dc64e4ce3eaf2c001a782a
Daniela Zapata <zapidan@gmail.com>
2014-01-18 19:38:12 -0600

    Add basic twitter updatestatus 140 chars

--- src/com/dano/yamba/StatusActivity.java
+++ src/com/dano/yamba/StatusActivity.java
@@ -1,19 +1,30 @@
 package com.dano.yamba;
 
+import com.marakana.android.yamba.clientlib.YambaClient;
+import com.marakana.android.yamba.clientlib.YambaClientException;
+
 import android.app.Activity;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class StatusActivity extends Activity implements OnClickListener {
 	
 	private static final String TAG = "StatusActivity";
 	private EditText editStatus;
 	private Button buttonTweet;
+	private TextView textCount;
+	private int defaultTextColor;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +33,40 @@ public class StatusActivity extends Activity implements OnClickListener {
 		
 		editStatus = (EditText)findViewById(R.id.editStatus);
 		buttonTweet = (Button)findViewById(R.id.buttonTweet);
+		textCount = (TextView)findViewById(R.id.textCount);
 		
-		buttonTweet.setOnClickListener(this);
+		buttonTweet.setOnClickListener(this);	
 		
+		defaultTextColor = textCount.getTextColors().getDefaultColor();
+		editStatus.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+				int after) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				int count = 140 - editStatus.length();
+				
+				textCount.setText(Integer.toString(count));
+				textCount.setTextColor(Color.GREEN);
+				if (count <10) {
+					textCount.setTextColor(Color.RED);
+				}
+//				else {
+//					textCount.setTextColor(defaultTextColor);
+//				}
+			}
+		});
 	}
 	
 	@Override
@@ -32,12 +74,42 @@ public class StatusActivity extends Activity implements OnClickListener {
 		String status = editStatus.getText().toString();
 		
 		Log.d(TAG, "onClicked with status: " + status);
+		
+		new PostTask().execute(status);
+	}
+	
+	private final class PostTask extends AsyncTask<String, Void, String> {
+
+		@Override
+		protected String doInBackground(String... params) {
+			YambaClient yambaCloud = new YambaClient("student", "password");
+			
+			try {
+				yambaCloud.postStatus(params[0]);
+				
+				return "Successfully posted";
+			}
+			catch (YambaClientException e) {
+				e.printStackTrace();
+				
+				return "Failed to post to yamba service";
+			}
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			
+			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG).show();
+		}
+		
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.status, menu);
+		
 		return true;
 	}
 	
