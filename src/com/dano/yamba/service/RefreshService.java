465929d60d9c41eee631c7fac7b2c9f07f6222a8
Daniela Zapata <zapidan@gmail.com>
2014-01-20 23:10:17 -0600

    Add db to store tweets

--- src/com/dano/yamba/service/RefreshService.java
+++ src/com/dano/yamba/service/RefreshService.java
@@ -2,17 +2,21 @@ package com.dano.yamba.service;
 
 import java.util.List;
 
-import com.marakana.android.yamba.clientlib.YambaClient;
-import com.marakana.android.yamba.clientlib.YambaClient.Status;
-import com.marakana.android.yamba.clientlib.YambaClientException;
-
 import android.app.IntentService;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Toast;
+
+import com.dano.yamba.database.DbHelper;
+import com.dano.yamba.database.StatusContract;
+import com.marakana.android.yamba.clientlib.YambaClient;
+import com.marakana.android.yamba.clientlib.YambaClient.Status;
+import com.marakana.android.yamba.clientlib.YambaClientException;
 
 public class RefreshService extends IntentService {
 	private static final String TAG = "RefreshService";
@@ -42,6 +46,10 @@ public class RefreshService extends IntentService {
 		
 		Log.d(TAG, "onHandleIntent");
 		
+		DbHelper dbHelper = new DbHelper(this);
+		SQLiteDatabase db = dbHelper.getWritableDatabase();
+		ContentValues values = new ContentValues();
+		
 		YambaClient cloud = new YambaClient(username, password);
 		
 		List<Status> timeline;
@@ -49,7 +57,13 @@ public class RefreshService extends IntentService {
 			timeline = cloud.getTimeline(20);
 			
 			for (Status status : timeline) {
-				Log.d(TAG, String.format("%s: %s",  status.getUser(), status.getMessage()));
+				values.clear();
+				values.put(StatusContract.Column.ID, status.getId());
+				values.put(StatusContract.Column.USER, status.getUser());
+				values.put(StatusContract.Column.MESSAGE, status.getMessage());
+				values.put(StatusContract.Column.CREATED_AT, status.getCreatedAt().getTime());
+				
+				db.insertWithOnConflict(StatusContract.TABLE, null, values, SQLiteDatabase.CONFLICT_IGNORE);
 			}
 		}
 		catch (YambaClientException e) {
